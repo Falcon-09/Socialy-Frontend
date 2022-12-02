750a62c4c01f030f92034b0644f7dbbd6465d689
Nipun_Mitra <57940556+Falcon-09@users.noreply.github.com>
2022-12-02 16:24:21 +0530

    Update User.jsx

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,19 +1,37 @@
+import axios from "axios";
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { followUser, unfollowUser } from "../../actions/UserAction";
+import { ChatRoute } from "../../Routes/ChatRoute";
 const User = ({ person }) => {
-  const publicFolder = "https://socialy-backend.herokuapp.com/images/";
+  const publicFolder = process.env.REACT_APP_PUBLIC_FOLDER;
   const { user } = useSelector((state) => state.authReducer.authData);
   const dispatch = useDispatch()
   
   const [following, setFollowing] = useState(
     person.followers.includes(user._id)
   );
-  const handleFollow = () => {
+  const handleFollow = async () => {
+
+    const chatExists = await axios.get(ChatRoute+`find/${user._id}/${person._id}`);
+    
+    if(!chatExists.data){
+      await axios.post(ChatRoute,{
+        senderId: user._id,
+        receiverId: person._id,
+        text: ""
+      })
+    }
+
     following
       ? dispatch(unfollowUser(person._id, user))
       : dispatch(followUser(person._id, user));
     setFollowing((prev) => !prev);
+
+    if(following){
+      console.log(ChatRoute+`${user._id}/${person._id}`);
+      await axios.delete(ChatRoute+`${user._id}/${person._id}`);
+    }
   };
   return (
     <div className="follower">
