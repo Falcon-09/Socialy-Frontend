3b2bca3327bd73a51ed4efd17dfb7b5b7acd365a
Nipun_Mitra <57940556+Falcon-09@users.noreply.github.com>
2022-12-03 00:55:27 +0530

    Update VerifyRoute.js

--- src/Routes/VerifyRoute.js
+++ src/Routes/VerifyRoute.js
@@ -1,4 +1,4 @@
-const base = "https://socialy-backend.onrender.com"
+const base = "https://socialy-backend-production.up.railway.app"
 
 export const VerifyRoute = `${base}/verify`
 export const ResendOtpRoute = `${base}/verify/resend`
