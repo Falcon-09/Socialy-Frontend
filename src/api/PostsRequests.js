e1a5da42281e8bc667015999b3304396276db512
Nipun_Mitra <57940556+Falcon-09@users.noreply.github.com>
2022-12-02 16:46:39 +0530

    Update PostsRequests.js

--- src/api/PostsRequests.js
+++ src/api/PostsRequests.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const API = axios.create({ baseURL: 'https://socialy-backend.herokuapp.com' });
+const API = axios.create({ baseURL: 'https://socialy-backend.onrender.com' });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
