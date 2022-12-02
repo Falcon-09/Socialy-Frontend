f4f11a024d2c57b0ad5c94b11b0862f81aa562e1
Nipun_Mitra <57940556+Falcon-09@users.noreply.github.com>
2022-12-02 16:45:12 +0530

    Update ChatRequests.js

--- src/api/ChatRequests.js
+++ src/api/ChatRequests.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const API = axios.create({ baseURL: 'https://socialy-backend.herokuapp.com' });
+const API = axios.create({ baseURL: 'https://socialy-backend.onrender.com' });
 
 export const createChat = (data) => API.post('/chat/', data);
 
